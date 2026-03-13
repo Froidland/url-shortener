887967ac515a80d21084fb8110c3dbfb4ba39d96
Francisco Hauva <phauva@proton.me>
2026-03-13 20:25:38 -0300

    build: bump all package versions and remove posthog

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,15 +1,4 @@
-import posthog from 'posthog-js';
-import { browser, dev } from '$app/environment';
-import { env } from '$env/dynamic/public';
-
 export const load = async ({ data }) => {
-	if (browser && !dev) {
-		posthog.init(env.PUBLIC_POSTHOG_WRITE_API_KEY, {
-			api_host: env.PUBLIC_POSTHOG_API_HOST,
-			person_profiles: 'always'
-		});
-	}
-
 	return {
 		...data
 	};
